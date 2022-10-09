f7994a2725666e64ad49fd5e7e6989ac25654fd5
Natanael Neto <natanael.eneas2018@hotmail.com>
2022-10-09 08:49:47 -0300

    sprint-1 - 03 - adiciona dependências de testes e eslint

---
+++ src/tests/sandbox.test.js
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe('simple test', () => {
+  it('runs', () => {
+    expect(1).toBeTruthy();
+  });
+});
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    include:['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude:['**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**'],
+    globals: true,
+    environment: 'jsdom'
+  }
 })
